edd25a1cb3280d482b0e9888c1da3c40590df2cc
Mel Padden <paddentech@gmail.com>
2025-04-08 22:57:55 +0200

    Working at about 3 seconds per file

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import argparse
 import os
 # load the libraries that we'll use
-from mutagen.mp3 import MP3
 from mutagen.mp4 import MP4
 from mutagen.easyid3 import EasyID3
 import mutagen.id3
@@ -11,6 +10,7 @@ from mutagen.id3 import ID3, TIT2, TIT3, TALB, TPE1, TRCK, TYER
 from pathlib import Path
 import json
 from pydub import AudioSegment
+from mutagen.id3 import ID3, ID3NoHeaderError
 
 import glob
 
@@ -41,7 +41,10 @@ def main():
     parser.add_argument(
         "--excluded",
         nargs="+",
-        default=[],
+        default=["White Noise", "Audiobooks", "Podcasts", ".DS_Store"],
+        dest="excluded",
+        # type=str,
+        # # type=lambda x: x.split(","),
         help="List of directories to exclude"
     )
     args = parser.parse_args()
@@ -53,61 +56,158 @@ def main():
 
     print(f"Music path: {music_path}")
     print(f"Output path: {output_path}")
-    print(f"Excluded directories: {excluded}")
+    # print(f"Excluded directories: {excluded}")
 
     # Call your function to process music files
-    get_music(music_path, output_path)
+    get_music_structure(music_path, output_path, excluded)
 
 
-def get_music(music_path, output_path):
-    music_set = set()
-    for root, dirs, files in os.walk(music_path):
-        print(f"Processing directory: {root}")
-        # print(f"Files: {files}")
-        # print(f"Directories: {dirs}")
-        break
+def get_music_structure(music_path, output_path, excluded=[]):
+    print(f"Processing root directory: {music_path}")
+    for artist in os.listdir(music_path):
+        if artist in excluded:
+            print(f"Excluding artist: {artist}")
+            continue
+        artist_path = os.path.join(music_path, artist)
+        if os.path.isdir(artist_path):            
+            print(f"Processing artist directory: {artist}")
+            for album in os.listdir(artist_path):
+                if album in excluded:
+                    print(f"Excluding album: {album}")
+                    continue
+                
+                print(f"Processing album: {album}")
+                album_path = os.path.join(artist_path, album)
+                out_dir = os.path.join(output_path, f"{artist}-{album}")
+                
+                if not os.path.exists(out_dir):
+                    os.makedirs(out_dir)
+                
+                process_album(artist, album, album_path, out_dir)
 
-    for filename in Path(music_path).rglob("*.m4a"):
-        print(filename)
 
-        # Load the M4A file
-        audio = MP4(filename)
-
-        artist = audio.get("\xa9ART", ["Unknown Artist"])[0]
-        title = audio.get("\xa9nam", ["Unknown Title"])[0]
-        album = audio.get("\xa9alb", ["Unknown Album"])[0]
-        track_number = audio.get("trkn", ["Unknown track Number"])[0][0]
-        music = Music(artist, album, title, track_number)
-        music_set.add(music)
-        
-        # Print the metadata
-        print(f"Title: {title}")
-        print(f"Artist: {artist}")
-        print(f"Album: {album}")
-        print(f"Track Number: {track_number}")
-
-        # Access the audio properties
-        # print(f"Bitrate: {audio.info.bitrate / 1000:.2f} kbps")
-        # print(f"Sample Rate: {audio.info.sample_rate} Hz")
-        # print(f"Channels: {audio.info.channels}")
-
-        # Print duration
-        # print(f"Duration: {audio.info.length:.2f} seconds")
-
-        out_dir = os.path.join(output_path, f"{artist}-{album}")
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-
-        # Create the output file path
+def process_album(artist, album, input_path, out_dir):
+    print(f"Processing album: {album} from artist: {artist}")
+    for filepath in Path(input_path).rglob("*.m4a"):
+        filename = os.path.basename(filepath)
+        track_number = filename[:2]
         output_file=os.path.join(
-            out_dir, f"{str(track_number).zfill(2)}-{title}.mp3")
-        print(f"Output file: {output_file}")
+            out_dir, f"{track_number}-{filename[3:].rstrip(".m4a")}.mp3")
+        if os.path.exists(output_file):
+            print(f"Skipping file: {output_file}")
+            continue
+        
+        print(f"Creating file: {output_file}")
+        # Load the M4A metadata
+        mp4_metadata = MP4(filepath)
+
+        # print(f"Processing file: {mp4file.filename}")
+        artist = mp4_metadata.get("\xa9ART", ["Unknown Artist"])[0]
+        title = mp4_metadata.get("\xa9nam", ["Unknown Title"])[0]
+        album = mp4_metadata.get("\xa9alb", ["Unknown Album"])[0]
+        track_number = mp4_metadata.get("trkn", ["Unknown track Number"])[0][0]
+        genre = mp4_metadata.get("\xa9gen", ["Unknown Genre"])[0]
+        date = mp4_metadata.get("\xa9day", ["Unknown Date"])[0]
+        # disk_number = mp4_metadata.get("disk", ["Unknown Disk Number"])[0][0]
+        # comment = mp4_metadata.get("\xa9cmt", ["Unknown Comment"])[0]
+        #music = Music(artist, album, title, track_number)
+        #music_set.add(music)        
+        
+        # Create the output file path
+        #print(f"Output file: {output_file}")
         # Load the .m4a file
-        audio=AudioSegment.from_file(filename, format="m4a")
+        mp4file=AudioSegment.from_file(filepath, format="m4a")
 
         # Export as .mp3
-        audio.export(output_file, format="mp3")
+        mp4file.export(output_file, format="mp3")
+        
+        # Add metadata using mutagen
+        try:
+            tags = EasyID3(output_file)
+        except ID3NoHeaderError:
+            tags = EasyID3()
+            tags.save(output_file)  # Add ID3 header if not present
+            tags = EasyID3(output_file)
 
+        tags["title"] = title
+        tags["artist"] = artist
+        tags["tracknumber"] = str(track_number)
+        tags["album"] = album
+        tags["genre"] = genre
+        tags["date"] = date
+
+        tags.save()
+
+    print(f"Completed album: {album} from artist: {artist}")
+
+def get_music(music_path, output_path, excluded=[]):
+    print(f"Processing root directory: {music_path}")
+    music_set = set()
+    for root, dirs, files in os.walk(music_path):
+        
+        for dir in dirs:
+            if dir in excluded:
+                print(f"Excluding directory: {dir}")
+                continue
+            
+            print(f"Processing directory: {dir}")
+                    
+            for filename in Path(dir).rglob("*.m4a"):
+                #print(filename)
+
+                # Load the M4A file
+                mp4file = MP4(filename)
+
+                # print(f"Processing file: {mp4file.filename}")
+                artist = mp4file.get("\xa9ART", ["Unknown Artist"])[0]
+                title = mp4file.get("\xa9nam", ["Unknown Title"])[0]
+                album = mp4file.get("\xa9alb", ["Unknown Album"])[0]
+                track_number = mp4file.get("trkn", ["Unknown track Number"])[0][0]
+                genre = mp4file.get("\xa9gen", ["Unknown Genre"])[0]
+                date = mp4file.get("\xa9day", ["Unknown Date"])[0]
+                disk_number = mp4file.get("disk", ["Unknown Disk Number"])[0][0]
+                comment = mp4file.get("\xa9cmt", ["Unknown Comment"])[0]
+                music = Music(artist, album, title, track_number)
+                music_set.add(music)
+                
+                out_dir = os.path.join(output_path, f"{artist}-{album}")
+                output_file=os.path.join(
+                    out_dir, f"{str(track_number).zfill(2)}-{os.path.basename(filename).rstrip(".m4a")}.mp3")
+                if os.path.exists(output_file):
+                    #print(f"File already exists: {output_file}")
+                    continue
+                
+                if not os.path.exists(out_dir):
+                    os.makedirs(out_dir)
+
+                # Create the output file path
+                print(f"Output file: {output_file}")
+                # Load the .m4a file
+                mp4file=AudioSegment.from_file(filename, format="m4a")
+
+                # Export as .mp3
+                mp4file.export(output_file, format="mp3")
+                
+                # Add metadata using mutagen
+                try:
+                    tags = EasyID3(output_file)
+                except ID3NoHeaderError:
+                    tags = EasyID3()
+                    tags.save(output_file)  # Add ID3 header if not present
+                    tags = EasyID3(output_file)
+
+                tags["title"] = title
+                tags["artist"] = artist
+                tags["tracknumber"] = str(track_number)
+                tags["album"] = album
+                tags["genre"] = genre
+                tags["date"] = date
+                tags["comment"] = comment
+                tags["disk"] = str(disk_number)
+
+                tags.save()
+
+            print(f"Completed directory: {dir}")
         # # Access ID3 tags (metadata)
         # for tag in audio.tags:
         #     print(f"{tag}: {audio.tags[tag]}")
